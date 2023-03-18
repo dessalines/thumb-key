346f03dd11fbb255c7c51afab2ee1c2e3b8f9738
Dessalines <dessalines@users.noreply.github.com>
2023-03-17 23:17:07 -0400

    Numeric mode input fix (#24)
    * Adding release notes.
    
    * Automatically bring up numeric keyboard on numeric inputs.
    
    - Fixes #4

--- app/src/main/java/com/dessalines/thumbkey/IMEService.kt
+++ app/src/main/java/com/dessalines/thumbkey/IMEService.kt
@@ -1,12 +1,16 @@
 package com.dessalines.thumbkey
 
 import android.inputmethodservice.InputMethodService
+import android.util.Log
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import androidx.lifecycle.*
 import androidx.savedstate.SavedStateRegistry
 import androidx.savedstate.SavedStateRegistryController
 import androidx.savedstate.SavedStateRegistryOwner
 import androidx.savedstate.setViewTreeSavedStateRegistryOwner
+import com.dessalines.thumbkey.utils.TAG
+import com.dessalines.thumbkey.utils.getKeyboardMode
 
 class IMEService :
     InputMethodService(),
@@ -14,10 +18,12 @@ class IMEService :
     ViewModelStoreOwner,
     SavedStateRegistryOwner {
 
-    override fun onCreateInputView(): View {
-        val liveSettings = (application as ThumbkeyApplication).appSettingsRepository.appSettings
-        val view = ComposeKeyboardView(this, liveSettings)
+    private fun setupView(): View {
+        val liveSettings = (application as ThumbkeyApplication).appSettingsRepository
+            .appSettings
 
+        val startMode = getKeyboardMode(this)
+        val view = ComposeKeyboardView(this, liveSettings, startMode)
         window?.window?.decorView?.let { decorView ->
             ViewTreeLifecycleOwner.set(decorView, this)
             ViewTreeViewModelStoreOwner.set(decorView, this)
@@ -28,7 +34,18 @@ class IMEService :
             ViewTreeViewModelStoreOwner.set(it, this)
             view.setViewTreeSavedStateRegistryOwner(this)
         }
+        Log.d(TAG, "Thumbkey initialized to mode: $startMode")
         return view
+    }
+
+    /**
+     * This is called every time the keyboard is brought up.
+     * You can't use onCreate, because that can't pick up new numeric inputs
+     */
+    override fun onStartInput(attribute: EditorInfo?, restarting: Boolean) {
+        super.onStartInput(attribute, restarting)
+        val view = this.setupView()
+        this.setInputView(view)
     }
 
     // Lifecycle Methods
