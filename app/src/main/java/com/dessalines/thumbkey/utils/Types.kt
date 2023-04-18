80b383d9a79b445966534f4243e41cd366af62f9
Dessalines <dessalines@users.noreply.github.com>
2023-04-18 12:25:48 -0400

    Adding polish layout. Fixes #173 (#174)

--- app/src/main/java/com/dessalines/thumbkey/utils/Types.kt
+++ app/src/main/java/com/dessalines/thumbkey/utils/Types.kt
@@ -96,6 +96,7 @@ enum class KeyboardLayout(val title: String) {
     ThumbKeyFRv1("Thumb-Key Français v1"),
     ThumbKeyITv1("Thumb-Key italiano v1"),
     ThumbKeyNLv1("Thumb-Key Nederlands v1"),
+    ThumbKeyPLv2("Thumb-Key polski v2"),
     ThumbKeyPTv1("Thumb-Key português v1"),
     ThumbkeyRUv1("Thumb-Key русский v1"),
     ThumbkeyUKv1("Thumb-Key українська v1"),
