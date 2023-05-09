3808d207fa8e3fb42886069f93dbec33ea497633
Simon Slater <sslater11@gmail.com>
2023-05-09 01:35:30 +0100

    Display the keyboard layout's name on screen when the user switches layouts.

--- app/src/main/java/com/dessalines/thumbkey/ComposeKeyboardView.kt
+++ app/src/main/java/com/dessalines/thumbkey/ComposeKeyboardView.kt
@@ -2,6 +2,7 @@ package com.dessalines.thumbkey
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.widget.Toast
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.platform.AbstractComposeView
@@ -9,6 +10,7 @@ import androidx.lifecycle.lifecycleScope
 import com.dessalines.thumbkey.db.AppSettingsRepository
 import com.dessalines.thumbkey.ui.components.keyboard.KeyboardScreen
 import com.dessalines.thumbkey.ui.theme.ThumbkeyTheme
+import com.dessalines.thumbkey.utils.KeyboardLayout
 import com.dessalines.thumbkey.utils.keyboardLayoutsSetFromString
 import kotlinx.coroutines.launch
 
@@ -43,6 +45,9 @@ class ComposeKeyboardView(
                         nextLayout?.let { layout ->
                             val s2 = s.copy(keyboardLayout = layout)
                             settingsRepo.update(s2)
+                            // Display the new layout's name on the screen
+                            val layoutName = KeyboardLayout.values().find { it.index == nextLayout }?.title
+                            Toast.makeText(context, "$layoutName", Toast.LENGTH_SHORT).show()
                         }
                     }
                 },
