98320d1eb572dfc9c3682e3b604fc88d3548890f
Francisco Trigo <francisco.trigomijes@gmail.com>
2023-06-05 22:35:24 -0500

    Srcery color theme (#275)
    * Srcery color theme
    
    Made with Material Design theme builder, based on https://srcery.sh/
    
    * Did ./gradlew formatKotlin

--- app/src/main/java/com/dessalines/thumbkey/ui/theme/Color.kt
+++ app/src/main/java/com/dessalines/thumbkey/ui/theme/Color.kt
@@ -266,3 +266,134 @@ fun green(): Pair<ColorScheme, ColorScheme> {
     )
     return Pair(light, dark)
 }
+
+fun srcery(): Pair<ColorScheme, ColorScheme> {
+    val md_theme_light_primary = Color(0xFF6D5E00)
+    val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+    val md_theme_light_primaryContainer = Color(0xFFFCE365)
+    val md_theme_light_onPrimaryContainer = Color(0xFF211B00)
+    val md_theme_light_secondary = Color(0xFF655E40)
+    val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+    val md_theme_light_secondaryContainer = Color(0xFFEDE2BC)
+    val md_theme_light_onSecondaryContainer = Color(0xFF201C04)
+    val md_theme_light_tertiary = Color(0xFF42664F)
+    val md_theme_light_onTertiary = Color(0xFFFFFFFF)
+    val md_theme_light_tertiaryContainer = Color(0xFFC4ECCF)
+    val md_theme_light_onTertiaryContainer = Color(0xFF002110)
+    val md_theme_light_error = Color(0xFFBA1A1A)
+    val md_theme_light_errorContainer = Color(0xFFFFDAD6)
+    val md_theme_light_onError = Color(0xFFFFFFFF)
+    val md_theme_light_onErrorContainer = Color(0xFF410002)
+    val md_theme_light_background = Color(0xFFFFFBFF)
+    val md_theme_light_onBackground = Color(0xFF1D1B16)
+    val md_theme_light_surface = Color(0xFFFFFBFF)
+    val md_theme_light_onSurface = Color(0xFF1D1B16)
+    val md_theme_light_surfaceVariant = Color(0xFFE9E2D0)
+    val md_theme_light_onSurfaceVariant = Color(0xFF4A4739)
+    val md_theme_light_outline = Color(0xFF7C7768)
+    val md_theme_light_inverseOnSurface = Color(0xFFF6F0E7)
+    val md_theme_light_inverseSurface = Color(0xFF32302A)
+    val md_theme_light_inversePrimary = Color(0xFFDEC64C)
+    val md_theme_light_shadow = Color(0xFF000000)
+    val md_theme_light_surfaceTint = Color(0xFF6D5E00)
+    val md_theme_light_outlineVariant = Color(0xFFCDC6B4)
+    val md_theme_light_scrim = Color(0xFF000000)
+
+    val md_theme_dark_primary = Color(0xFFDEC64C)
+    val md_theme_dark_onPrimary = Color(0xFF393000)
+    val md_theme_dark_primaryContainer = Color(0xFF524600)
+    val md_theme_dark_onPrimaryContainer = Color(0xFFFCE365)
+    val md_theme_dark_secondary = Color(0xFFD0C7A2)
+    val md_theme_dark_onSecondary = Color(0xFF363016)
+    val md_theme_dark_secondaryContainer = Color(0xFF4D472B)
+    val md_theme_dark_onSecondaryContainer = Color(0xFFEDE2BC)
+    val md_theme_dark_tertiary = Color(0xFFA9D0B4)
+    val md_theme_dark_onTertiary = Color(0xFF133724)
+    val md_theme_dark_tertiaryContainer = Color(0xFF2B4E39)
+    val md_theme_dark_onTertiaryContainer = Color(0xFFC4ECCF)
+    val md_theme_dark_error = Color(0xFFFFB4AB)
+    val md_theme_dark_errorContainer = Color(0xFF93000A)
+    val md_theme_dark_onError = Color(0xFF690005)
+    val md_theme_dark_onErrorContainer = Color(0xFFFFDAD6)
+    val md_theme_dark_background = Color(0xFF1D1B16)
+    val md_theme_dark_onBackground = Color(0xFFE7E2D9)
+    val md_theme_dark_surface = Color(0xFF1D1B16)
+    val md_theme_dark_onSurface = Color(0xFFE7E2D9)
+    val md_theme_dark_surfaceVariant = Color(0xFF4A4739)
+    val md_theme_dark_onSurfaceVariant = Color(0xFFCDC6B4)
+    val md_theme_dark_outline = Color(0xFF969080)
+    val md_theme_dark_inverseOnSurface = Color(0xFF1D1B16)
+    val md_theme_dark_inverseSurface = Color(0xFFE7E2D9)
+    val md_theme_dark_inversePrimary = Color(0xFF6D5E00)
+    val md_theme_dark_shadow = Color(0xFF000000)
+    val md_theme_dark_surfaceTint = Color(0xFFDEC64C)
+    val md_theme_dark_outlineVariant = Color(0xFF4A4739)
+    val md_theme_dark_scrim = Color(0xFF000000)
+
+    val seed = Color(0xFF1C1B19)
+
+    val light = lightColorScheme(
+        primary = md_theme_light_primary,
+        onPrimary = md_theme_light_onPrimary,
+        primaryContainer = md_theme_light_primaryContainer,
+        onPrimaryContainer = md_theme_light_onPrimaryContainer,
+        secondary = md_theme_light_secondary,
+        onSecondary = md_theme_light_onSecondary,
+        secondaryContainer = md_theme_light_secondaryContainer,
+        onSecondaryContainer = md_theme_light_onSecondaryContainer,
+        tertiary = md_theme_light_tertiary,
+        onTertiary = md_theme_light_onTertiary,
+        tertiaryContainer = md_theme_light_tertiaryContainer,
+        onTertiaryContainer = md_theme_light_onTertiaryContainer,
+        error = md_theme_light_error,
+        errorContainer = md_theme_light_errorContainer,
+        onError = md_theme_light_onError,
+        onErrorContainer = md_theme_light_onErrorContainer,
+        background = md_theme_light_background,
+        onBackground = md_theme_light_onBackground,
+        surface = md_theme_light_surface,
+        onSurface = md_theme_light_onSurface,
+        surfaceVariant = md_theme_light_surfaceVariant,
+        onSurfaceVariant = md_theme_light_onSurfaceVariant,
+        outline = md_theme_light_outline,
+        inverseOnSurface = md_theme_light_inverseOnSurface,
+        inverseSurface = md_theme_light_inverseSurface,
+        inversePrimary = md_theme_light_inversePrimary,
+        surfaceTint = md_theme_light_surfaceTint,
+        outlineVariant = md_theme_light_outlineVariant,
+        scrim = md_theme_light_scrim,
+    )
+
+    val dark = darkColorScheme(
+        primary = md_theme_dark_primary,
+        onPrimary = md_theme_dark_onPrimary,
+        primaryContainer = md_theme_dark_primaryContainer,
+        onPrimaryContainer = md_theme_dark_onPrimaryContainer,
+        secondary = md_theme_dark_secondary,
+        onSecondary = md_theme_dark_onSecondary,
+        secondaryContainer = md_theme_dark_secondaryContainer,
+        onSecondaryContainer = md_theme_dark_onSecondaryContainer,
+        tertiary = md_theme_dark_tertiary,
+        onTertiary = md_theme_dark_onTertiary,
+        tertiaryContainer = md_theme_dark_tertiaryContainer,
+        onTertiaryContainer = md_theme_dark_onTertiaryContainer,
+        error = md_theme_dark_error,
+        errorContainer = md_theme_dark_errorContainer,
+        onError = md_theme_dark_onError,
+        onErrorContainer = md_theme_dark_onErrorContainer,
+        background = md_theme_dark_background,
+        onBackground = md_theme_dark_onBackground,
+        surface = md_theme_dark_surface,
+        onSurface = md_theme_dark_onSurface,
+        surfaceVariant = md_theme_dark_surfaceVariant,
+        onSurfaceVariant = md_theme_dark_onSurfaceVariant,
+        outline = md_theme_dark_outline,
+        inverseOnSurface = md_theme_dark_inverseOnSurface,
+        inverseSurface = md_theme_dark_inverseSurface,
+        inversePrimary = md_theme_dark_inversePrimary,
+        surfaceTint = md_theme_dark_surfaceTint,
+        outlineVariant = md_theme_dark_outlineVariant,
+        scrim = md_theme_dark_scrim,
+    )
+    return Pair(light, dark)
+}
