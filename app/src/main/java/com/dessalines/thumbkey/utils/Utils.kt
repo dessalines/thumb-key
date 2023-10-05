3b5dc4ffe08cbd7fc328da8b127189a783b47408
Wade <wadewtaylor@live.co.uk>
2023-10-06 08:12:34 +1000

    woops

--- app/src/main/java/com/dessalines/thumbkey/utils/Utils.kt
+++ app/src/main/java/com/dessalines/thumbkey/utils/Utils.kt
@@ -25,7 +25,6 @@ import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.Dp
