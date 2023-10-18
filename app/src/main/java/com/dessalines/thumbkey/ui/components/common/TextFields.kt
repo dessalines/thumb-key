9c7c382fcf8ba76b53c9c83f6f22fc6f89b6408b
Dessalines <tyhou13@gmx.com>
2023-10-18 12:47:11 -0400

    Running format.

--- app/src/main/java/com/dessalines/thumbkey/ui/components/common/TextFields.kt
+++ app/src/main/java/com/dessalines/thumbkey/ui/components/common/TextFields.kt
@@ -1,7 +1,6 @@
 package com.dessalines.thumbkey.ui.components.common
 
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
