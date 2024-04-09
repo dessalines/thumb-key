9f49f3ad16bad8b3326f56b326a9fc83af66d012
Dessalines <tyhou13@gmx.com>
2024-04-09 19:48:14 -0400

    lowercase.

--- app/src/main/java/com/dessalines/thumbkey/keyboards/VNThumbKey.kt
+++ app/src/main/java/com/dessalines/thumbkey/keyboards/VNThumbKey.kt
@@ -707,7 +707,7 @@ val KB_VN_THUMBKEY_SHIFTED =
 
 val KB_VN_THUMBKEY: KeyboardDefinition =
     KeyboardDefinition(
-        title = "tiếng Việt thumb-key",
+        title = "tiếng việt thumb-key",
         modes =
             KeyboardDefinitionModes(
                 main = KB_VN_THUMBKEY_MAIN,
