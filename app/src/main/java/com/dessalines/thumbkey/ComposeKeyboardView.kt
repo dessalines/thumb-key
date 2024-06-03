e70063f31532b12dc32323e8fa444e3f653618fc
pxeemo <shayans31516@gmail.com>
2024-06-03 16:33:50 +0330

    Force LTR KeyboardScreen

--- app/src/main/java/com/dessalines/thumbkey/ComposeKeyboardView.kt
+++ app/src/main/java/com/dessalines/thumbkey/ComposeKeyboardView.kt
@@ -4,9 +4,12 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.widget.Toast
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.platform.AbstractComposeView
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.lifecycle.lifecycleScope
 import com.dessalines.thumbkey.db.AppSettingsRepository
 import com.dessalines.thumbkey.ui.components.keyboard.KeyboardScreen
@@ -30,42 +33,46 @@ class ComposeKeyboardView(
         ThumbkeyTheme(
             settings = settings,
         ) {
-            KeyboardScreen(
-                settings = settings,
-                onSwitchLanguage = {
-                    ctx.lifecycleScope.launch {
-                        // Cycle to the next keyboard
-                        val state = settingsState.value
-                        state?.let { s ->
+            CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) {
+                KeyboardScreen(
+                    settings = settings,
+                    onSwitchLanguage = {
+                        ctx.lifecycleScope.launch {
+                            // Cycle to the next keyboard
+                            val state = settingsState.value
+                            state?.let { s ->
 
-                            val layouts = keyboardLayoutsSetFromDbIndexString(s.keyboardLayouts).toList()
-                            val currentLayout = s.keyboardLayout
-                            val index = layouts.map { it.ordinal }.indexOf(currentLayout)
-                            val nextIndex = (index + 1).mod(layouts.size)
-                            val nextLayout = layouts.getOrNull(nextIndex)
-                            nextLayout?.let { layout ->
-                                val s2 = s.copy(keyboardLayout = layout.ordinal)
-                                settingsRepo.update(s2)
+                                val layouts =
+                                    keyboardLayoutsSetFromDbIndexString(s.keyboardLayouts).toList()
+                                val currentLayout = s.keyboardLayout
+                                val index = layouts.map { it.ordinal }.indexOf(currentLayout)
+                                val nextIndex = (index + 1).mod(layouts.size)
+                                val nextLayout = layouts.getOrNull(nextIndex)
+                                nextLayout?.let { layout ->
+                                    val s2 = s.copy(keyboardLayout = layout.ordinal)
+                                    settingsRepo.update(s2)
 
-                                // Display the new layout's name on the screen
-                                val layoutName = layout.keyboardDefinition.title
-                                Toast.makeText(context, layoutName, Toast.LENGTH_SHORT).show()
+                                    // Display the new layout's name on the screen
+                                    val layoutName = layout.keyboardDefinition.title
+                                    Toast.makeText(context, layoutName, Toast.LENGTH_SHORT)
+                                        .show()
+                                }
                             }
                         }
-                    }
-                },
-                onSwitchPosition = {
-                    ctx.lifecycleScope.launch {
-                        // Cycle to the next position
-                        val state = settingsState.value
-                        state?.let { s ->
-                            val nextPosition = (s.position + 1).mod(3)
-                            val s2 = s.copy(position = nextPosition)
-                            settingsRepo.update(s2)
+                    },
+                    onSwitchPosition = {
+                        ctx.lifecycleScope.launch {
+                            // Cycle to the next position
+                            val state = settingsState.value
+                            state?.let { s ->
+                                val nextPosition = (s.position + 1).mod(3)
+                                val s2 = s.copy(position = nextPosition)
+                                settingsRepo.update(s2)
+                            }
                         }
-                    }
-                },
-            )
+                    },
+                )
+            }
         }
     }
 }
