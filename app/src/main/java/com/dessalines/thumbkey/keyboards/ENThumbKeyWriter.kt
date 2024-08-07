faa74be69d6706dc46fd9c6abdea734ee5d2b4a5
Defrobe <to.the.top@mail.ru>
2024-08-07 19:48:25 +0300

    Update ENThumbKeyWriter.kt
    Added swipe-and-return gesture to capitalize words for EN Writer keyboard

--- app/src/main/java/com/dessalines/thumbkey/keyboards/ENThumbKeyWriter.kt
+++ app/src/main/java/com/dessalines/thumbkey/keyboards/ENThumbKeyWriter.kt
@@ -270,7 +270,13 @@ val KB_EN_THUMBKEY_WRITER_MAIN =
                                 KeyC(
                                     display = KeyDisplay.IconDisplay(Icons.Outlined.ArrowDropUp),
                                     action = KeyAction.ToggleShiftMode(true),
+                                    swipeReturnAction = KeyAction.ToggleCurrentWordCapitalization(true),
                                     color = ColorVariant.MUTED,
+                                ),
+                            SwipeDirection.BOTTOM to
+                                KeyC(
+                                    action = KeyAction.ToggleShiftMode(false),
+                                    swipeReturnAction = KeyAction.ToggleCurrentWordCapitalization(false),
                                 ),
                             SwipeDirection.TOP_LEFT to
                                 KeyC(
@@ -742,12 +748,14 @@ val KB_EN_THUMBKEY_WRITER_SHIFTED =
                                     display = KeyDisplay.IconDisplay(Icons.Outlined.KeyboardCapslock),
                                     capsModeDisplay = KeyDisplay.IconDisplay(Icons.Outlined.Copyright),
                                     action = KeyAction.ToggleCapsLock,
+                                    swipeReturnAction = KeyAction.ToggleCurrentWordCapitalization(true),
                                     color = ColorVariant.MUTED,
                                 ),
                             SwipeDirection.BOTTOM to
                                 KeyC(
                                     display = KeyDisplay.IconDisplay(Icons.Outlined.ArrowDropDown),
                                     action = KeyAction.ToggleShiftMode(false),
+                                    swipeReturnAction = KeyAction.ToggleCurrentWordCapitalization(false),
                                     color = ColorVariant.MUTED,
                                 ),
                             SwipeDirection.TOP_LEFT to
