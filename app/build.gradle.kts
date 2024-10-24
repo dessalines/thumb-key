b8547b57ce7fd372e34b79b8eaf0abae14330bc1
Dessalines <dessalines@users.noreply.github.com>
2024-10-24 18:02:07 -0400

    Convert gradle to kotlin (#1124)
    * Update plugin com.google.devtools.ksp to v2.0.21-1.0.26
    
    * Converting gradle to kotlin build.
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

---
+++ app/build.gradle.kts
@@ -0,0 +1,135 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+    id("com.google.devtools.ksp")
+}
+
+android {
+    buildToolsVersion = "34.0.0"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.dessalines.thumbkey"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 117
+        versionName = "3.4.16"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+        ksp { arg("room.schemaLocation", "$projectDir/schemas") }
+    }
+
+    // Necessary for izzyondroid releases
+    dependenciesInfo {
+        // Disables dependency metadata when building APKs.
+        includeInApk = false
+        // Disables dependency metadata when building Android App Bundles.
+        includeInBundle = false
+    }
+
+    if (project.hasProperty("RELEASE_STORE_FILE")) {
+        signingConfigs {
+            register("release") {
+                storeFile = file(project.property("RELEASE_STORE_FILE")!!)
+                storePassword = project.property("RELEASE_STORE_PASSWORD") as String?
+                keyAlias = project.property("RELEASE_KEY_ALIAS") as String?
+                keyPassword = project.property("RELEASE_KEY_PASSWORD") as String?
+
+                // Optional, specify signing versions used
+                enableV1Signing = true
+                enableV2Signing = true
+            }
+        }
+    }
+    buildTypes {
+        release {
+            signingConfig = if (project.hasProperty("RELEASE_STORE_FILE")) {
+                signingConfigs.getByName("release")
+            } else {
+                signingConfigs.getByName("debug")
+            }
+
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                // Includes the default ProGuard rules files that are packaged with
+                // the Android Gradle plugin. To learn more, go to the section about
+                // R8 configuration files.
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+
+                // Includes a local, custom Proguard rules file
+                "proguard-rules.pro"
+            )
+        }
+        debug {
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = " (DEBUG)"
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+        freeCompilerArgs = listOf("-Xjvm-default=all-compatibility", "-opt-in=kotlin.RequiresOptIn")
+    }
+    buildFeatures {
+        compose = true
+    }
+    namespace = "com.dessalines.thumbkey"
+}
+
+dependencies {
+    // Exporting / importing DB helper
+    implementation("com.github.dessalines:room-db-export-import:0.1.0")
+
+    // Compose BOM
+    implementation(platform("androidx.compose:compose-bom:2024.10.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended-desktop:1.7.4")
+    implementation("androidx.compose.material3:material3-window-size-class")
+    implementation("androidx.compose.ui:ui-tooling")
+    implementation("androidx.compose.runtime:runtime-livedata")
+
+    // Activities
+    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.activity:activity-ktx:1.9.3")
+
+    // LiveData
+    implementation("androidx.lifecycle:lifecycle-runtime-compose")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-compose:2.8.3")
+
+    // Emoji Picker
+    implementation("androidx.emoji2:emoji2-emojipicker:1.5.0")
+
+    // Markdown
+    implementation("com.github.jeziellago:compose-markdown:0.5.4")
+
+    // Preferences
+    implementation("me.zhanghai.compose.preference:library:1.1.1")
+
+    // Input switcher
+    implementation("com.louiscad.splitties:splitties-systemservices:3.0.0")
+    implementation("com.louiscad.splitties:splitties-views:3.0.0")
+
+    // Room
+    // To use Kotlin annotation processing tool
+    ksp("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-runtime:2.6.1")
+    annotationProcessor("androidx.room:room-compiler:2.6.1")
+
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:2.6.1")
+
+    // App compat
+    implementation("androidx.appcompat:appcompat:1.7.0")
+}
