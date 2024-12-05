92a0c05f2d15e3e1b49fb3e6080731904388baea
Dessalines <tyhou13@gmx.com>
2024-12-04 22:06:13 -0500

    Revert "Upgrading ksp."
    This reverts commit c588dc744015357fedfa088194f7ce0dd5c89364.

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,10 @@ buildscript {
 plugins {
     id("com.android.application") version "8.7.3" apply false
     id("com.android.library") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
     id("org.jmailen.kotlinter") version "4.5.0" apply false
-    id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
+    id("com.google.devtools.ksp") version "2.0.21-1.0.28" apply false
 }
 
 subprojects {
