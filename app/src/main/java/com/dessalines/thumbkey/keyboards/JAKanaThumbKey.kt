02b6f242c24cb00e10beb2f80572d4b1c85057de
K0baU <63403741+K0baU@users.noreply.github.com>
2024-12-11 14:05:54 +0900

    Fix JAKanaThumbKey.kt

--- app/src/main/java/com/dessalines/thumbkey/keyboards/JAKanaThumbKey.kt
+++ app/src/main/java/com/dessalines/thumbkey/keyboards/JAKanaThumbKey.kt
@@ -230,7 +230,7 @@ val KB_JA_KANA_THUMBKEY_KATAKANA =
                     center =
                         KeyC(
                             display = KeyDisplay.TextDisplay("あ"),
-                            action = ShiftAndCapsLock,
+                            action = ShiftAndCapsLock(true),
                             size = LARGE,
                             color = MUTED,
                         ),
