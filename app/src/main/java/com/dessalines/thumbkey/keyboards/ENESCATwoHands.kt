44067502029fb9d6d58fb26f74a7c93bf9c45460
José Reyes <josreyes53@gmail.com>
2025-03-07 17:47:48 -0400

    Add ENESCATwoHands layout

---
+++ app/src/main/java/com/dessalines/thumbkey/keyboards/ENESCATwoHands.kt
@@ -0,0 +1,396 @@
+@file:Suppress("ktlint:standard:no-wildcard-imports")
+
+package com.dessalines.thumbkey.keyboards
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.*
+import com.dessalines.thumbkey.utils.*
+import com.dessalines.thumbkey.utils.ColorVariant.*
+import com.dessalines.thumbkey.utils.FontSizeVariant.*
+import com.dessalines.thumbkey.utils.KeyAction.*
+import com.dessalines.thumbkey.utils.SwipeNWay.*
+
+val KB_EN_ES_CA_TWO_HANDS_MAIN =
+    KeyboardC(
+        listOf(
+            listOf(
+                KeyItemC(
+                    center = KeyC("s", size = LARGE),
+                    swipeType = FOUR_WAY_DIAGONAL,
+                    bottomRight = KeyC("w"),
+                ),
+                KeyItemC(
+                    center = KeyC("r", size = LARGE),
+                    topLeft = KeyC("¡"),
+                    topRight = KeyC("!"),
+                    bottom = KeyC("g"),
+                ),
+                KeyItemC(
+                    center = KeyC("o", size = LARGE),
+                    bottomLeft = KeyC("u"),
+                    right = KeyC("ó"),
+                    bottomRight = KeyC("ú"),
+                    bottom = KeyC("ü"),
+                    left = KeyC("ò"),
+                    topLeft = KeyC("¿"),
+                    topRight = KeyC("?"),
+                ),
+                EMOJI_KEY_ITEM,
+                KeyItemC(
+                    center = KeyC("s", size = LARGE),
+                    swipeType = FOUR_WAY_DIAGONAL,
+                    bottomRight = KeyC("w"),
+                ),
+                KeyItemC(
+                    center = KeyC("r", size = LARGE),
+                    topLeft = KeyC("¡"),
+                    topRight = KeyC("!"),
+                    bottom = KeyC("g"),
+                ),
+                KeyItemC(
+                    center = KeyC("o", size = LARGE),
+                    bottomLeft = KeyC("u"),
+                    right = KeyC("ó"),
+                    bottomRight = KeyC("ú"),
+                    bottom = KeyC("ü"),
+                    left = KeyC("ò"),
+                    topLeft = KeyC("¿"),
+                    topRight = KeyC("?"),
+                ),
+            ),
+            listOf(
+                KeyItemC(
+                    center = KeyC("n", size = LARGE),
+                    swipeType = TWO_WAY_HORIZONTAL,
+                    right = KeyC("m"),
+                    left = KeyC("ñ"),
+                ),
+                KeyItemC(
+                    center = KeyC("h", size = LARGE),
+                    topLeft = KeyC("j"),
+                    top = KeyC("q"),
+                    topRight = KeyC("b"),
+                    right = KeyC("p"),
+                    bottomRight = KeyC("y"),
+                    bottom = KeyC("x"),
+                    bottomLeft = KeyC("v"),
+                    left = KeyC("k"),
+                ),
+                KeyItemC(
+                    center = KeyC("a", size = LARGE),
+                    topLeft = KeyC("à"),
+                    topRight = KeyC("á"),
+                    left = KeyC("l"),
+                    bottom =
+                        KeyC(
+                            ToggleShiftMode(false),
+                            swipeReturnAction = ToggleCurrentWordCapitalization(false),
+                        ),
+                    bottomLeft = KeyC("l·l"),
+                    top =
+                        KeyC(
+                            display = KeyDisplay.IconDisplay(Icons.Outlined.ArrowDropUp),
+                            action = ToggleShiftMode(true),
+                            swipeReturnAction = ToggleCurrentWordCapitalization(true),
+                            color = MUTED,
+                        ),
+                ),
+                NUMERIC_KEY_ITEM,
+                KeyItemC(
+                    center = KeyC("n", size = LARGE),
+                    swipeType = TWO_WAY_HORIZONTAL,
+                    right = KeyC("m"),
+                    left = KeyC("ñ"),
+                ),
+                KeyItemC(
+                    center = KeyC("h", size = LARGE),
+                    topLeft = KeyC("j"),
+                    top = KeyC("q"),
+                    topRight = KeyC("b"),
+                    right = KeyC("p"),
+                    bottomRight = KeyC("y"),
+                    bottom = KeyC("x"),
+                    bottomLeft = KeyC("v"),
+                    left = KeyC("k"),
+                ),
+                KeyItemC(
+                    center = KeyC("a", size = LARGE),
+                    topLeft = KeyC("à"),
+                    topRight = KeyC("á"),
+                    left = KeyC("l"),
+                    bottom =
+                        KeyC(
+                            ToggleShiftMode(false),
+                            swipeReturnAction = ToggleCurrentWordCapitalization(false),
+                        ),
+                    bottomLeft = KeyC("l·l"),
+                    top =
+                        KeyC(
+                            display = KeyDisplay.IconDisplay(Icons.Outlined.ArrowDropUp),
+                            action = ToggleShiftMode(true),
+                            swipeReturnAction = ToggleCurrentWordCapitalization(true),
+                            color = MUTED,
+                        ),
+                ),
+            ),
+            listOf(
+                KeyItemC(
+                    center = KeyC("t", size = LARGE),
+                    swipeType = FOUR_WAY_DIAGONAL,
+                    topRight = KeyC("c"),
+                    bottomRight = KeyC("ç"),
+                ),
+                KeyItemC(
+                    center = KeyC("i", size = LARGE),
+                    top = KeyC("f"),
+                    topRight = KeyC("'", color = MUTED),
+                    right = KeyC("z"),
+                    bottomRight = KeyC("-", color = MUTED),
+                    bottom = KeyC(".", color = MUTED),
+                    bottomLeft = KeyC("*", color = MUTED),
+                    left = KeyC("í"),
+                    topLeft = KeyC("ï"),
+                ),
+                KeyItemC(
+                    center = KeyC("e", size = LARGE),
+                    topLeft = KeyC("d"),
+                    right = KeyC("é"),
+                    left = KeyC("è"),
+                ),
+                BACKSPACE_KEY_ITEM,
+                KeyItemC(
+                    center = KeyC("t", size = LARGE),
+                    swipeType = FOUR_WAY_DIAGONAL,
+                    topRight = KeyC("c"),
+                    bottomRight = KeyC("ç"),
+                ),
+                KeyItemC(
+                    center = KeyC("i", size = LARGE),
+                    top = KeyC("f"),
+                    topRight = KeyC("'", color = MUTED),
+                    right = KeyC("z"),
+                    bottomRight = KeyC("-", color = MUTED),
+                    bottom = KeyC(".", color = MUTED),
+                    bottomLeft = KeyC("*", color = MUTED),
+                    left = KeyC("í"),
+                    topLeft = KeyC("ï"),
+                ),
+                KeyItemC(
+                    center = KeyC("e", size = LARGE),
+                    topLeft = KeyC("d"),
+                    right = KeyC("é"),
+                    left = KeyC("è"),
+                ),
+            ),
+            listOf(
+                RETURN_KEY_ITEM,
+                BACKSPACE_KEY_ITEM,
+                SPACEBAR_KEY_ITEM,
+                BACKSPACE_KEY_ITEM,
+                RETURN_KEY_ITEM,
+            ),
+        ),
+    )
+
+val KB_EN_ES_CA_TWO_HANDS_SHIFTED =
+    KeyboardC(
+        listOf(
+            listOf(
+                KeyItemC(
+                    center = KeyC("S", size = LARGE),
+                    swipeType = FOUR_WAY_DIAGONAL,
+                    bottomRight = KeyC("W"),
+                ),
+                KeyItemC(
+                    center = KeyC("R", size = LARGE),
+                    topLeft = KeyC("¡"),
+                    topRight = KeyC("!"),
+                    bottom = KeyC("G"),
+                ),
+                KeyItemC(
+                    center = KeyC("O", size = LARGE),
+                    bottomLeft = KeyC("U"),
+                    right = KeyC("Ó"),
+                    bottomRight = KeyC("Ú"),
+                    bottom = KeyC("Ü"),
+                    left = KeyC("Ò"),
+                    topLeft = KeyC("¿"),
+                    topRight = KeyC("?"),
+                ),
+                EMOJI_KEY_ITEM,
+                KeyItemC(
+                    center = KeyC("S", size = LARGE),
+                    swipeType = FOUR_WAY_DIAGONAL,
+                    bottomRight = KeyC("W"),
+                ),
+                KeyItemC(
+                    center = KeyC("R", size = LARGE),
+                    topLeft = KeyC("¡"),
+                    topRight = KeyC("!"),
+                    bottom = KeyC("G"),
+                ),
+                KeyItemC(
+                    center = KeyC("O", size = LARGE),
+                    bottomLeft = KeyC("U"),
+                    right = KeyC("Ó"),
+                    bottomRight = KeyC("Ú"),
+                    bottom = KeyC("Ü"),
+                    left = KeyC("Ò"),
+                    topLeft = KeyC("¿"),
+                    topRight = KeyC("?"),
+                ),
+            ),
+            listOf(
+                KeyItemC(
+                    center = KeyC("N", size = LARGE),
+                    swipeType = TWO_WAY_HORIZONTAL,
+                    right = KeyC("M"),
+                    left = KeyC("Ñ"),
+                ),
+                KeyItemC(
+                    center = KeyC("H", size = LARGE),
+                    topLeft = KeyC("J"),
+                    top = KeyC("Q"),
+                    topRight = KeyC("B"),
+                    right = KeyC("P"),
+                    bottomRight = KeyC("Y"),
+                    bottom = KeyC("X"),
+                    bottomLeft = KeyC("V"),
+                    left = KeyC("K"),
+                ),
+                KeyItemC(
+                    center = KeyC("A", size = LARGE),
+                    topLeft = KeyC("À"),
+                    topRight = KeyC("Á"),
+                    left = KeyC("L"),
+                    bottomLeft = KeyC("L·L"),
+                    top =
+                        KeyC(
+                            display = KeyDisplay.IconDisplay(Icons.Outlined.KeyboardCapslock),
+                            capsModeDisplay = KeyDisplay.IconDisplay(Icons.Outlined.Copyright),
+                            action = ToggleCapsLock,
+                            swipeReturnAction = ToggleCurrentWordCapitalization(true),
+                            color = MUTED,
+                        ),
+                    bottom =
+                        KeyC(
+                            display = KeyDisplay.IconDisplay(Icons.Outlined.ArrowDropDown),
+                            action = ToggleShiftMode(false),
+                            swipeReturnAction = ToggleCurrentWordCapitalization(false),
+                            color = MUTED,
+                        ),
+                ),
+                NUMERIC_KEY_ITEM,
+                KeyItemC(
+                    center = KeyC("N", size = LARGE),
+                    swipeType = TWO_WAY_HORIZONTAL,
+                    right = KeyC("M"),
+                    left = KeyC("Ñ"),
+                ),
+                KeyItemC(
+                    center = KeyC("H", size = LARGE),
+                    topLeft = KeyC("J"),
+                    top = KeyC("Q"),
+                    topRight = KeyC("B"),
+                    right = KeyC("P"),
+                    bottomRight = KeyC("Y"),
+                    bottom = KeyC("X"),
+                    bottomLeft = KeyC("V"),
+                    left = KeyC("K"),
+                ),
+                KeyItemC(
+                    center = KeyC("A", size = LARGE),
+                    topLeft = KeyC("À"),
+                    topRight = KeyC("Á"),
+                    left = KeyC("L"),
+                    bottomLeft = KeyC("L·L"),
+                    top =
+                        KeyC(
+                            display = KeyDisplay.IconDisplay(Icons.Outlined.KeyboardCapslock),
+                            capsModeDisplay = KeyDisplay.IconDisplay(Icons.Outlined.Copyright),
+                            action = ToggleCapsLock,
+                            swipeReturnAction = ToggleCurrentWordCapitalization(true),
+                            color = MUTED,
+                        ),
+                    bottom =
+                        KeyC(
+                            display = KeyDisplay.IconDisplay(Icons.Outlined.ArrowDropDown),
+                            action = ToggleShiftMode(false),
+                            swipeReturnAction = ToggleCurrentWordCapitalization(false),
+                            color = MUTED,
+                        ),
+                ),
+            ),
+            listOf(
+                KeyItemC(
+                    center = KeyC("T", size = LARGE),
+                    swipeType = FOUR_WAY_DIAGONAL,
+                    topRight = KeyC("C"),
+                    bottomRight = KeyC("Ç"),
+                ),
+                KeyItemC(
+                    center = KeyC("I", size = LARGE),
+                    top = KeyC("F"),
+                    topRight = KeyC("'", color = MUTED),
+                    right = KeyC("Z"),
+                    bottomRight = KeyC("-", color = MUTED),
+                    bottom = KeyC(".", color = MUTED),
+                    bottomLeft = KeyC("*", color = MUTED),
+                    left = KeyC("Í"),
+                    topLeft = KeyC("Ï"),
+                ),
+                KeyItemC(
+                    center = KeyC("E", size = LARGE),
+                    topLeft = KeyC("D"),
+                    right = KeyC("É"),
+                    left = KeyC("È"),
+                ),
+                BACKSPACE_KEY_ITEM,
+                KeyItemC(
+                    center = KeyC("T", size = LARGE),
+                    swipeType = FOUR_WAY_DIAGONAL,
+                    topRight = KeyC("C"),
+                    bottomRight = KeyC("Ç"),
+                ),
+                KeyItemC(
+                    center = KeyC("I", size = LARGE),
+                    top = KeyC("F"),
+                    topRight = KeyC("'", color = MUTED),
+                    right = KeyC("Z"),
+                    bottomRight = KeyC("-", color = MUTED),
+                    bottom = KeyC(".", color = MUTED),
+                    bottomLeft = KeyC("*", color = MUTED),
+                    left = KeyC("Í"),
+                    topLeft = KeyC("Ï"),
+                ),
+                KeyItemC(
+                    center = KeyC("E", size = LARGE),
+                    topLeft = KeyC("D"),
+                    right = KeyC("É"),
+                    left = KeyC("È"),
+                ),
+            ),
+            listOf(
+                RETURN_KEY_ITEM,
+                BACKSPACE_KEY_ITEM,
+                SPACEBAR_KEY_ITEM,
+                BACKSPACE_KEY_ITEM,
+                RETURN_KEY_ITEM,
+            ),
+        ),
+    )
+
+val KB_EN_ES_CA_TWO_HANDS: KeyboardDefinition =
+    KeyboardDefinition(
+        title = "english español català two-hands",
+        modes =
+            KeyboardDefinitionModes(
+                main = KB_EN_ES_CA_TWO_HANDS_MAIN,
+                shifted = KB_EN_ES_CA_TWO_HANDS_SHIFTED,
+                numeric = TWO_HANDS_NUMERIC_KEYBOARD,
+            ),
+        settings =
+            KeyboardDefinitionSettings(
+                autoCapitalizers = arrayOf(::autoCapitalizeI, ::autoCapitalizeIApostrophe),
+            ),
+    )
