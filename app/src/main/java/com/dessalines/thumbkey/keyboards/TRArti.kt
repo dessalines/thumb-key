21548b62c80c7c6b3472611090db6edc339127e6
Caleb Maclennan <caleb@alerque.com>
2025-07-12 17:13:27 +0300

    Reduce error-prone duplicate symbols with copied and modified keys

--- app/src/main/java/com/dessalines/thumbkey/keyboards/TRArti.kt
+++ app/src/main/java/com/dessalines/thumbkey/keyboards/TRArti.kt
@@ -190,228 +190,159 @@ val KB_TR_ARTI_MAIN =
     )
 
 val KB_TR_ARTI_SHIFTED =
-    KeyboardC(
-        listOf(
-            listOf(
-                EMOJI_BACK_KEY_ITEM,
-                KeyItemC(
-                    swipeType = EIGHT_WAY,
-                    center = KeyC("S", size = LARGE),
-                    topLeft = KeyC("`", color = MUTED),
-                    top = KeyC("*", color = MUTED),
-                    topRight = KeyC("~", color = MUTED),
-                    right = KeyC("≤", color = MUTED),
-                    bottomRight = KeyC("W"),
-                    bottom = KeyC("Ş"),
-                    bottomLeft = KeyC("&", color = MUTED),
-                    left = KeyC("%", color = MUTED),
-                ),
-                KeyItemC(
-                    swipeType = EIGHT_WAY,
-                    center = KeyC("R", size = LARGE),
-                    topLeft = KeyC("\\", color = MUTED),
-                    top = KeyC("^", color = MUTED),
-                    topRight = KeyC("/", color = MUTED),
-                    right = KeyC("-", color = MUTED),
-                    bottomRight = KeyC("Ğ"),
-                    bottom = KeyC("G"),
-                    bottomLeft = KeyC("_", color = MUTED),
-                    left = KeyC("≥", color = MUTED),
-                ),
-                KeyItemC(
-                    swipeType = EIGHT_WAY,
-                    center = KeyC("O", size = LARGE),
-                    topLeft = KeyC("=", color = MUTED),
-                    top = KeyC("×", color = MUTED),
-                    topRight = KeyC("+", color = MUTED),
-                    right = KeyC("÷", color = MUTED),
-                    bottomRight = KeyC("≈", color = MUTED),
-                    bottom = KeyC("Ö"),
-                    bottomLeft = KeyC("U"),
-                    left = KeyC("Ü"),
-                ),
-                NUMERIC_KEY_ITEM,
-            ),
-            listOf(
-                SPACEBAR_ALL_DIRECTIONS_ARTI,
-                KeyItemC(
-                    swipeType = EIGHT_WAY,
-                    center = KeyC("N", size = LARGE),
-                    topLeft = KeyC("{", color = MUTED),
-                    top = KeyC("$", color = MUTED),
-                    topRight = KeyC("₺", color = MUTED),
-                    right = KeyC("M"),
-                    bottomRight = KeyC("₸", color = MUTED),
-                    bottom = KeyC("€", color = MUTED),
-                    bottomLeft = KeyC("[", color = MUTED),
-                    left = KeyC("(", color = MUTED),
-                ),
-                KeyItemC(
-                    swipeType = EIGHT_WAY,
-                    center = KeyC("H", size = LARGE),
-                    topLeft = KeyC("J"),
-                    top = KeyC("Q"),
-                    topRight = KeyC("B"),
-                    right = KeyC("P"),
-                    bottomRight = KeyC("Y"),
-                    bottom = KeyC("X"),
-                    bottomLeft = KeyC("V"),
-                    left = KeyC("K"),
-                ),
-                KeyItemC(
-                    swipeType = EIGHT_WAY,
-                    center = KeyC("A", size = LARGE),
-                    topLeft = KeyC("Â"),
-                    top = KeyC(";", color = MUTED),
-                    topRight = KeyC("}", color = MUTED),
-                    right = KeyC(")", color = MUTED),
-                    bottomRight = KeyC("]", color = MUTED),
-                    bottom = KeyC(":", color = MUTED),
-                    bottomLeft = KeyC("|", color = MUTED),
-                    left = KeyC("L"),
-                ),
-                SPACEBAR_ALL_SYMBOLS_ARTI.copy(
-                    top =
-                        KeyC(
-                            display = KeyDisplay.IconDisplay(Icons.Outlined.KeyboardCapslock),
-                            capsModeDisplay = KeyDisplay.TextDisplay(""),
-                            action = ShiftAndCapsLock(true),
-                            size = LARGE,
-                        ),
-                ),
-            ),
-            listOf(
-                BACKSPACE_KEY_ITEM,
-                KeyItemC(
-                    swipeType = EIGHT_WAY,
-                    center = KeyC("T", size = LARGE),
-                    topLeft = KeyC("\"", color = MUTED),
-                    top = KeyC("Ç"),
-                    topRight = KeyC("C"),
-                    right = KeyC("!", color = MUTED),
-                    bottomRight = KeyC("‹", color = MUTED),
-                    bottom = KeyC("«", color = MUTED),
-                    bottomLeft = KeyC("←", color = MUTED),
-                    left = KeyC("<", color = MUTED),
-                ),
-                KeyItemC(
-                    swipeType = EIGHT_WAY,
-                    center = KeyC("I", size = LARGE),
-                    topLeft = KeyC("İ"),
-                    top = KeyC("F"),
-                    topRight = KeyC("°", color = MUTED),
-                    right = KeyC("Z"),
-                    bottom = KeyC("‽", color = MUTED),
-                    left = KeyC("?", color = MUTED),
-                ),
-                KeyItemC(
-                    swipeType = EIGHT_WAY,
-                    center = KeyC("E", size = LARGE),
-                    topLeft = KeyC("D"),
-                    top = KeyC("#", color = MUTED),
-                    topRight = KeyC("'", color = MUTED),
-                    right = KeyC(">", color = MUTED),
-                    bottomRight = KeyC("→", color = MUTED),
-                    bottom = KeyC("»", color = MUTED),
-                    bottomLeft = KeyC("›", color = MUTED),
-                    left = KeyC("@", color = MUTED),
-                ),
-                RETURN_KEY_ARTI,
-            ),
-        ),
+    KB_TR_ARTI_MAIN.copy().alterKeys(
+        Triple(0, 1) { key ->
+            key.copy(
+                center = KeyC("S", size = LARGE),
+                bottomRight = KeyC("W"),
+                bottom = KeyC("Ş"),
+            )
+        },
+        Triple(0, 2) { key ->
+            key.copy(
+                center = KeyC("R", size = LARGE),
+                bottomRight = KeyC("Ğ"),
+                bottom = KeyC("G"),
+            )
+        },
+        Triple(0, 3) { key ->
+            key.copy(
+                center = KeyC("O", size = LARGE),
+                bottom = KeyC("Ö"),
+                bottomLeft = KeyC("U"),
+                left = KeyC("Ü"),
+            )
+        },
+        Triple(1, 1) { key ->
+            key.copy(
+                center = KeyC("N", size = LARGE),
+                right = KeyC("M"),
+            )
+        },
+        Triple(1, 2) { key ->
+            key.copy(
+                center = KeyC("H", size = LARGE),
+                topLeft = KeyC("J"),
+                top = KeyC("Q"),
+                topRight = KeyC("B"),
+                right = KeyC("P"),
+                bottomRight = KeyC("Y"),
+                bottom = KeyC("X"),
+                bottomLeft = KeyC("V"),
+                left = KeyC("K"),
+            )
+        },
+        Triple(1, 3) { key ->
+            key.copy(
+                center = KeyC("A", size = LARGE),
+                topLeft = KeyC("Â"),
+                top = KeyC(";", color = MUTED),
+                bottom = KeyC(":", color = MUTED),
+                left = KeyC("L"),
+            )
+        },
+        Triple(2, 1) { key ->
+            key.copy(
+                center = KeyC("T", size = LARGE),
+                top = KeyC("Ç"),
+                topRight = KeyC("C"),
+            )
+        },
+        Triple(2, 2) { key ->
+            key.copy(
+                center = KeyC("I", size = LARGE),
+                topLeft = KeyC("İ"),
+                top = KeyC("F"),
+                right = KeyC("Z"),
+            )
+        },
+        Triple(2, 3) { key ->
+            key.copy(
+                center = KeyC("E", size = LARGE),
+                topLeft = KeyC("D"),
+            )
+        },
     )
 
 val KB_TR_ARTI_NUMERIC =
-    KeyboardC(
-        listOf(
-            listOf(
-                KeyItemC(
-                    center = KeyC("0", size = LARGE),
-                ),
-                KeyItemC(
-                    center = KeyC("1", size = LARGE),
-                    topLeft = KeyC("`", color = MUTED),
-                    top = KeyC("*", color = MUTED),
-                    topRight = KeyC("~", color = MUTED),
-                    right = KeyC("≤", color = MUTED),
-                    bottomLeft = KeyC("&", color = MUTED),
-                    left = KeyC("%", color = MUTED),
-                ),
-                KeyItemC(
-                    center = KeyC("2", size = LARGE),
-                    topLeft = KeyC("\\", color = MUTED),
-                    top = KeyC("^", color = MUTED),
-                    topRight = KeyC("/", color = MUTED),
-                    right = KeyC("-"),
-                    bottomLeft = KeyC("_", color = MUTED),
-                    left = KeyC("≥", color = MUTED),
-                ),
-                KeyItemC(
-                    center = KeyC("3", size = LARGE),
-                    topLeft = KeyC("=", color = MUTED),
-                    top = KeyC("×", color = MUTED),
-                    topRight = KeyC("+", color = MUTED),
-                    right = KeyC("÷", color = MUTED),
-                    bottomRight = KeyC("≈", color = MUTED),
-                ),
-                ABC_KEY_ITEM,
-            ),
-            listOf(
-                SPACEBAR_ALL_DIRECTIONS_ARTI,
-                KeyItemC(
-                    center = KeyC("4", size = LARGE),
-                    topLeft = KeyC("{", color = MUTED),
-                    top = KeyC("$", color = MUTED),
-                    topRight = KeyC("₺", color = MUTED),
-                    bottomRight = KeyC("₸", color = MUTED),
-                    bottom = KeyC("€", color = MUTED),
-                    bottomLeft = KeyC("[", color = MUTED),
-                    left = KeyC("(", color = MUTED),
-                ),
-                KeyItemC(
-                    center = KeyC("5", size = LARGE),
-                ),
-                KeyItemC(
-                    center = KeyC("6", size = LARGE),
-                    top = KeyC(",", color = MUTED),
-                    topRight = KeyC("}", color = MUTED),
-                    right = KeyC(")", color = MUTED),
-                    bottomRight = KeyC("]", color = MUTED),
-                    bottom = KeyC(".", color = MUTED),
-                    bottomLeft = KeyC("|", color = MUTED),
-                ),
-                SPACEBAR_ALL_SYMBOLS_ARTI,
-            ),
-            listOf(
-                BACKSPACE_KEY_ITEM,
-                KeyItemC(
-                    center = KeyC("7", size = LARGE),
-                    topLeft = KeyC("\"", color = MUTED),
-                    right = KeyC("!", color = MUTED),
-                    bottomRight = KeyC("‹", color = MUTED),
-                    bottom = KeyC("«", color = MUTED),
-                    bottomLeft = KeyC("←", color = MUTED),
-                    left = KeyC("<", color = MUTED),
-                ),
-                KeyItemC(
-                    center = KeyC("8", size = LARGE),
-                    topRight = KeyC("°", color = MUTED),
-                    bottom = KeyC("‽", color = MUTED),
-                    left = KeyC("?", color = MUTED),
-                ),
-                KeyItemC(
-                    center = KeyC("9", size = LARGE),
-                    top = KeyC("#", color = MUTED),
-                    topRight = KeyC("'", color = MUTED),
-                    right = KeyC(">", color = MUTED),
-                    bottomRight = KeyC("→", color = MUTED),
-                    bottom = KeyC("»", color = MUTED),
-                    bottomLeft = KeyC("›", color = MUTED),
-                    left = KeyC("@", color = MUTED),
-                ),
-                RETURN_KEY_ARTI,
-            ),
-        ),
+    KB_TR_ARTI_MAIN.copy().alterKeys(
+        Triple(0, 0) { key ->
+            KeyItemC(
+                center = KeyC("0", size = LARGE),
+            )
+        },
+        Triple(0, 1) { key ->
+            key.copy(
+                center = KeyC("1", size = LARGE),
+                bottomRight = null,
+                bottom = null,
+            )
+        },
+        Triple(0, 2) { key ->
+            key.copy(
+                center = KeyC("2", size = LARGE),
+                bottomRight = null,
+                bottom = null,
+            )
+        },
+        Triple(0, 3) { key ->
+            key.copy(
+                center = KeyC("3", size = LARGE),
+                bottom = null,
+                bottomLeft = null,
+                left = null,
+            )
+        },
+        Triple(0, 4) { key ->
+            ABC_KEY_ITEM
+        },
+        Triple(1, 1) { key ->
+            key.copy(
+                center = KeyC("4", size = LARGE),
+                right = null,
+            )
+        },
+        Triple(1, 2) { key ->
+            key.copy(
+                center = KeyC("5", size = LARGE),
+                topLeft = null,
+                top = null,
+                topRight = null,
+                right = null,
+                bottomRight = null,
+                bottom = null,
+                bottomLeft = null,
+                left = null,
+            )
+        },
+        Triple(1, 3) { key ->
+            key.copy(
+                center = KeyC("6", size = LARGE),
+                topLeft = null,
+                left = null,
+            )
+        },
+        Triple(2, 1) { key ->
+            key.copy(
+                center = KeyC("7", size = LARGE),
+                top = null,
+                topRight = null,
+            )
+        },
+        Triple(2, 2) { key ->
+            key.copy(
+                center = KeyC("8", size = LARGE),
+                topLeft = null,
+                top = null,
+                right = null,
+            )
+        },
+        Triple(2, 3) { key ->
+            key.copy(
+                center = KeyC("9", size = LARGE),
+                topLeft = null,
+            )
+        },
     )
 
 val KB_TR_ARTI: KeyboardDefinition =
