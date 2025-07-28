b7a6ebb124475bdd6995c31cd609dde377ad2115
Dessalines <tyhou13@gmx.com>
2025-07-28 14:52:57 -0400

    Upping version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.dessalines.thumbkey"
         minSdk = 24
         targetSdk = 35
-        versionCode = 158
-        versionName = "5.0.3"
+        versionCode = 159
+        versionName = "5.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
