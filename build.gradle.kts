c3774f413f5c30b9f452bc468c22f217198c85ee
Dessalines <tyhou13@gmx.com>
2026-01-17 09:00:18 -0500

    Remove kotlin.android

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,6 @@ buildscript {
 plugins {
     id("com.android.application") version "8.13.2" apply false
     id("com.android.library") version "9.0.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.0" apply false
     id("org.jmailen.kotlinter") version "5.3.0" apply false
     id("com.google.devtools.ksp") version "2.3.4" apply false
